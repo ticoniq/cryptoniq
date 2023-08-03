da1730b52158a15c97b8937794ea4f737d9dd7bc
ticoniq_ <okolietochi@gmail>
2023-08-03 04:41:31 +0100

    updated

--- src/__test__/navbar.test.js
+++ src/__test__/navbar.test.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Card from '../components/Card';
+import Navbar from '../components/Card';
 
 describe('Card', () => {
   it('should render the card with the correct title', () => {
-    const { getByText } = render(<Card title="My Card" />);
+    const { getByText } = render(<Navbar title="My Card" />);
 
     expect(getByText('My Card')).toBeTruthy();
   });
 
   it('should render the card with the correct details', () => {
-    const { getByText } = render(<Card title="My Card" details="120" />);
+    const { getByText } = render(<Navbar title="My Card" details="120" />);
 
     expect(getByText('120')).toBeTruthy();
   });
